529d7a579541ea4f9429a622fa515d55e3ebb08e
Juan Carlos <calabazo50@outlook.com>
2018-11-26 07:50:15 -0700

    Nueva practica: Lectura Serial

---
+++ P6.1_LecturaSerial/P6.1_LecturaSerial.ino
@@ -0,0 +1,23 @@
+int ledPin = 13;
+int val = 0;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(19200);
+  Serial.println("Conalep");
+}
+
+void loop(){
+  val = Serial.read();
+  if(val>'0' && val<='9'){
+    val=val-'0';
+    delay(500);
+    for(int i=0; i<val; i++){
+      Serial.println("Encendido!");
+      digitalWrite(ledPin, HIGH);
+      delay(150);
+      digitalWrite(ledPin, LOW);
+      delay(150);
+    }
+  }
+}
